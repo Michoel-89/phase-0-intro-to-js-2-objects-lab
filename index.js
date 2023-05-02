3640d2e4591f6167e071697034efbd1ba7d34a1a
michoel <michoelr111@gmail.com>
2023-05-02 09:55:14 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+let employee = {
+    name: 'john'
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let n = {...employee}
+    n[ key] = value; 
+    return n;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    let n = {...employee}
+    delete n[key];
+    return n;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
